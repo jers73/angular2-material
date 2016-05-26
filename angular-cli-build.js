619a15444e2a75768bc778474fd827337ca666cf
jers73 <Jorge Reyes>
2016-05-26 16:58:29 -0700

    add menu

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -2,17 +2,23 @@
 
 var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 
-module.exports = function(defaults) {
-  return new Angular2App(defaults, {
-    vendorNpmFiles: [
-      'systemjs/dist/system-polyfills.js',
-      'systemjs/dist/system.src.js',
-      'zone.js/dist/**/*.+(js|js.map)',
-      'es6-shim/es6-shim.js',
-      'reflect-metadata/**/*.+(js|js.map)',
-      'rxjs/**/*.+(js|js.map)',
-      '@angular2-material/**/*.+(js|js.map)',
-        '@angular/**/*.+(js|js.map)'
-    ]
-  });
+module.exports = function (defaults) {
+    return new Angular2App(defaults, {
+        sassCompiler: {
+            includePaths: [
+                'src/styles',
+                'node_modules/@angular2-material'
+            ]
+        },
+        vendorNpmFiles: [
+            'systemjs/dist/system-polyfills.js',
+            'systemjs/dist/system.src.js',
+            'zone.js/dist/**/*.+(js|js.map)',
+            'es6-shim/es6-shim.js',
+            'reflect-metadata/**/*.+(js|js.map)',
+            'rxjs/**/*.+(js|js.map)',
+            '@angular2-material/**/*.+(js|js.map)',
+            '@angular/**/*.+(js|js.map)'
+        ]
+    });
 };
